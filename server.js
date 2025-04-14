964111bf214d4f3970ae0b3e418697950487acfa
NaguleshN <nagulesh.22cs@kct.ac.in>
2025-04-14 18:15:54 +0530

    Added Rate Limiting, Slow Down

--- server.js
+++ server.js
@@ -6,9 +6,25 @@ import operatorRoutes from './routes/operatorRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import loggingService from './services/loggingService.js';
+import rateLimit from "express-rate-limit";
+import slowDown from "express-slow-down";
 
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100 
+});
+
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 1,
+  delayMs: () => 2000,
+});
 
 const app = express();
+app.use(limiter);
+app.use(speedLimiter);
+
 connectDB();
 app.use(cookieParser());
 app.use(express.json())
